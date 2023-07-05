5ff50b9e33eff721b18cbfc1eadfcf5049f4d5fa
Jonatas <delimajonatas875@gmail.com>
2023-07-05 17:09:56 -0300

    liberando o cors

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from uris import routers
 
 app = Flask(__name__)
 app.register_blueprint(routers)
+CORS(app)
 
 
 if __name__ == '__main__':
--- uris.py
+++ uris.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
 from app.view import convert_audio, audio_transcribe
 
 routers = Blueprint('uri', __name__)
 
 
 @routers.route('/', methods=['GET'])
+@cross_origin()
 def index():
     response = jsonify(
         {
@@ -17,10 +19,12 @@ def index():
 
 
 @routers.route('/convert', methods=['POST'])
+@cross_origin()
 def convert():
     return convert_audio(request=request)
 
 
 @routers.route('/transcribe', methods=['POST'])
+@cross_origin()
 def transcribe():
     return audio_transcribe(request=request)
